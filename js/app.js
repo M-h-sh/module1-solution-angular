29d0bbe4440ecc7604eafadaca33cfe03f0752bf
Mthokozisi Hector Hlatshwayo <79380754+M-h-sh@users.noreply.github.com>
2022-08-26 11:21:13 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,20 +8,20 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.items = '';
-  $scope.mssg = 'Please enter data first';
+  $scope.msg = 'Please enter data first';
 
   $scope.checkText = function() {
     if ($scope.items.length == 0) {
-      $scope.mssg = 'Please enter data first';
+      $scope.msg = 'Please enter data first';
       return;
     } 
     else {
       let listItems = $scope.items.split(',').filter(value => value.trim().length!=0);
       if (listItems.length <= 3) {
-        $scope.mssg = 'Enjoy!';
+        $scope.msg = 'Enjoy!';
       }
       else {
-        $scope.mssg = 'Too much!';
+        $scope.msg = 'Too much!';
       }      
     }
   }
